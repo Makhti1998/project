7db79d66e23b06499e14a842360fb5bee2f0b835
Makhti <msayfidarow@gmail.com>
2025-01-13 16:58:37 +0300

    edit-slider

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
+  loop: true,
+  slideToClickedSlide: true,
+  centeredSlides: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
